437618f814228ba7a699790b6c31f57b3853dd25
emil aberg <emil.aberg@elev.ga.ntig.se>
2022-03-25 10:31:02 +0100

    a

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 
+
+
 var app = express();
 
 nunjucks.configure("views", {
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index.njk', { title: 'startsida' });
 });
+
+router.get('/signin', function(req, res, next) {
+  res.render('signin.njk', { title: 'signin' });
+});
+router.get('/signup', function(req, res, next) {
+  res.render('signup.njk', { title: 'signup' });
+});
+
+router.get('/content', function(req, res, next) {
+  res.render('content.njk', { title: 'content' });
+});
+
+
 
 module.exports = router;
