34c11a4d4aeadf11c58cc7db84586eca0e1779a0
emil aberg <emil.aberg@elev.ga.ntig.se>
2022-03-25 11:11:11 +0100

    harsch

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../database');
+const bcrypt = require('bcrypt');
+const { hash } = require('bcrypt');
 
 /* GET users listing. */
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index.njk', { title: 'startsida' });
 });
 
-router.get('/signin', function(req, res, next) {
+router.get('/signin', function (req, res, next) {
   res.render('signin.njk', { title: 'signin' });
 });
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function (req, res, next) {
   res.render('signup.njk', { title: 'signup' });
 });
 
-router.get('/content', function(req, res, next) {
+router.post('/signup', function (req, res, next) {
+  const name = req.body.name
+  const password = req.body.password
+
+  bcrypt.hash(password, 10, async function (err, hash) {
+    console.log(hash)
+    await pool
+      .promise()
+      .query("INSERT INTO users (name,password) VALUES (?,?)", [name, hash])
+      .then((response) => {
+        console.log(response);
+
+      })
+      .catch((err) => {
+        console.log(err);
+
+      });
+  });
+});
+
+router.get('/content', function (req, res, next) {
   res.render('content.njk', { title: 'content' });
 });
 
